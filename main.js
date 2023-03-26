77208cbd457c993f222932ac9e7f3e1cc2ef6633
XSlimeNB <116885083+W9pi3cZ1@users.noreply.github.com>
2023-03-26 15:41:47 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+var audio = new Audio("/media/xslimenb/新加卷/minecraft/PCL/Musics/Lipps Inc. - Funkytown.flac");
+var btn = document.querySelector("#Btn");
+var Time;
+var Time2 = new Array(0,0);
+var dayTime = Date();
+var dayTime2 = new Array(0,0);
+var flag = false;
+btn.addEventListener('click',() => {
+    flag = false;
+    Time = document.querySelector("#Time").value
+    Time2[0] = parseInt(Time.split(":")[0]);
+    Time2[1] = parseInt(Time.split(":")[1]);
+    console.log(Time2);
+});
+
+function ifTime(){
+    dayTime = new Date();
+    dayTime2[0] = dayTime.getHours();
+    dayTime2[1] = dayTime.getMinutes();
+    if(dayTime2[0] == Time2[0] && dayTime2[1] == Time2[1]){
+        console.log("true");
+        if(!flag){
+            audio.load();
+            audio.play();
+            flag=true;
+        }
+    }else{
+        console.log("false");
+    }
+}
+
+setInterval(ifTime,1000)
